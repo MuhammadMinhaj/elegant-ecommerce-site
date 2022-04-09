9a2f5651f11a58c2ed02139f3fac25f011af4097
Muhammad Minhaj <developer.mdminhaj@gmail.com>
2022-04-09 22:00:11 +0600

    feat(): Added dynamic popular product component

--- src/pages/shop.js
+++ src/pages/shop.js
@@ -1,5 +1,6 @@
 import Layout from '../components/Layout/Layout';
 import AllCategories from '../components/Shop/AllCategories/AllCategories';
+import PopularProduct from '../components/Shop/PopularProduct/PopularProduct';
 import Slider from '../components/Shop/Slider/Container';
 import TodayDeals from '../components/Shop/TodayDeals/TodayDeals';
 
@@ -9,6 +10,8 @@ function ShopPage() {
       <Slider />
       <AllCategories />
       <TodayDeals />
+      <PopularProduct title="MOST POPULAR FOR MAN" leftBannerImgSrc="/MostPopularForMan/MostPopularForMan.jpg" bannerTxtTitle="All Product Discount - 30%" />
+      <PopularProduct title="MOST POPULAR FOR WOMEN" leftBannerImgSrc="/MostPopularForMan/MostPopularForWomen.jpg" bannerTxtTitle="Discount On Summer Collection" />
     </Layout>
   );
 }
