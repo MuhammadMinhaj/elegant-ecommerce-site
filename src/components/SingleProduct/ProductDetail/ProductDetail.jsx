5e017925e481cba0acabf2df78080c04d63533a5
Muhammad Minhaj <developer.mdminhaj@gmail.com>
2022-04-10 16:51:28 +0600

    feat(): Added product detail for single product

---
+++ src/components/SingleProduct/ProductDetail/ProductDetail.jsx
@@ -0,0 +1,33 @@
+import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
+import ShopIcon from '@mui/icons-material/Shop';
+import {
+  Box, Button, Rating, Typography, useMediaQuery, useTheme
+} from '@mui/material';
+import List from './List';
+import QuantityBtns from './QuantityBtns';
+
+function ProductDetail() {
+  const theme = useTheme();
+  const isMD = useMediaQuery(theme.breakpoints.down('md'));
+  return (
+    <Box>
+      <Typography variant="h4" component="h1">Product Title</Typography>
+      <Rating />
+      <Box py="1rem">
+        <Typography variant="h4" component="span" color="error">$436.46</Typography>
+      </Box>
+      <Box maxWidth={isMD ? '100%' : '400px'} width="100%">
+        <List />
+        <QuantityBtns />
+        <Box display="flex">
+          <Button startIcon={<AddShoppingCartIcon />} color="secondary" size="large" fullWidth>Add to cart</Button>
+          <Box width="1rem" />
+          <Button startIcon={<ShopIcon />} color="secondary" variant="outlined" size="large" fullWidth>Buy Now</Button>
+        </Box>
+      </Box>
+
+    </Box>
+  );
+}
+
+export default ProductDetail;
