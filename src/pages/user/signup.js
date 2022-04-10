39e127d46f0401ddce3bcf640aa0f4b8f59b7eb0
Muhammad Minhaj <developer.mdminhaj@gmail.com>
2022-04-10 20:06:14 +0600

    feat(): Added signup page

---
+++ src/pages/user/signup.js
@@ -0,0 +1,13 @@
+import { } from '@mui/material';
+import Layout from '../../components/Layout/Layout';
+import UserSignUp from '../../components/User/Signup/Signup';
+
+function UserSignUpPage() {
+  return (
+    <Layout>
+      <UserSignUp />
+    </Layout>
+  );
+}
+
+export default UserSignUpPage;
