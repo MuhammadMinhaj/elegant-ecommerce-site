3b6bbb13ed6e550d319b63a85284c4f634825732
Muhammad Minhaj <developer.mdminhaj@gmail.com>
2022-04-10 22:05:15 +0600

    feat(): User signup & singin page has been completed

---
+++ src/pages/user/login.js
@@ -0,0 +1,13 @@
+import { } from '@mui/material';
+import Layout from '../../components/Layout/Layout';
+import Login from '../../components/User/Login/Login';
+
+function UserLoginPage() {
+  return (
+    <Layout>
+      <Login />
+    </Layout>
+  );
+}
+
+export default UserLoginPage;
