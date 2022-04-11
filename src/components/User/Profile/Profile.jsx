c145bb59115794d885d23c7eca806783970c2896
Muhammad Minhaj <developer.mdminhaj@gmail.com>
2022-04-11 21:58:05 +0600

    feat(): Added carditem in user profile

--- src/components/User/Profile/Profile.jsx
+++ src/components/User/Profile/Profile.jsx
@@ -1,7 +1,11 @@
+import LocalShippingIcon from '@mui/icons-material/LocalShipping';
+import PaymentsIcon from '@mui/icons-material/Payments';
 import PersonSharpIcon from '@mui/icons-material/PersonSharp';
+import ShoppingBagOutlinedIcon from '@mui/icons-material/ShoppingBagOutlined';
 import { Box, Divider, Grid } from '@mui/material';
 import Title from '../Common/Title';
 import Layout from '../Layout/Layout';
+import CardItem from './CardItem';
 import User from './User';
 
 function Profile() {
@@ -11,19 +15,18 @@ function Profile() {
         <Title title="My Profile" icon={<PersonSharpIcon sx={{ color: 'inherit', fontSize: 'inherit' }} />} />
         <Divider />
         <Box p="1rem">
-
           <Grid container spacing={2}>
-            <Grid item sm={12} md={6}>
+            <Grid item xs={12} sm={12} md={12} lg={6}>
               <User />
             </Grid>
-            <Grid item sx={12} sm={6} md={2}>
-              item 1
+            <Grid item xs={6} sm={6} md={4} lg={2}>
+              <CardItem count="7" title="All Orders" icon={<ShoppingBagOutlinedIcon fontSize="inherit" color="inherit" />} />
             </Grid>
-            <Grid item sx={12} sm={6} md={2}>
-              item 2
+            <Grid item xs={6} sm={6} md={4} lg={2}>
+              <CardItem count="4" title="Awaiting Payments" icon={<PaymentsIcon fontSize="inherit" color="inherit" />} />
             </Grid>
-            <Grid item sx={12} sm={6} md={2}>
-              item 3
+            <Grid item xs={12} sm={6} md={4} lg={2}>
+              <CardItem count="7" title="Awaiting Delivery" icon={<LocalShippingIcon fontSize="inherit" color="inherit" />} />
             </Grid>
           </Grid>
         </Box>
