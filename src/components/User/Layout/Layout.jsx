552ce0bd9c879286bc57c8fd269d1b1b39b75367
Muhammad Minhaj <developer.mdminhaj@gmail.com>
2022-04-11 20:10:38 +0600

    feat(): Added user in profile section

--- src/components/User/Layout/Layout.jsx
+++ src/components/User/Layout/Layout.jsx
@@ -1,12 +1,12 @@
 import { Box, Grid } from '@mui/material';
 import Sidebar from '../Sidebar/Sidebar';
 
-function Layout({ children }) {
+function Layout({ children, currentPath }) {
   return (
     <Box className="container" py="2rem">
       <Grid container spacing={2}>
         <Grid item xs={12} sm={12} md={4} lg={3}>
-          <Sidebar />
+          <Sidebar currentPath={currentPath} />
         </Grid>
         <Grid item xs={12} sm={12} md={8} lg={9}>
           {children}
