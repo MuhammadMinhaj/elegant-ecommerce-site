419c33fcf6a6edc437e06938e4d522e74c69fa99
Muhammad Minhaj <developer.mdminhaj@gmail.com>
2022-04-11 12:53:16 +0600

    Updated

--- src/components/NewHeader/MainHeader/Brand.jsx
+++ src/components/NewHeader/MainHeader/Brand.jsx
@@ -1,10 +1,12 @@
 import { Box } from '@mui/material';
+import { useRouter } from 'next/router';
 
 function Barnd() {
+  const router = useRouter();
   return (
     <Box>
       <Box className="container">
-        <Box component="img" src="/images/site-logo.png" width="200px" height="auto" alt="Brand name" />
+        <Box component="img" src="/images/site-logo.png" width="200px" height="auto" alt="Brand name" sx={{ cursor: 'pointer' }} onClick={() => router.push('/')} />
       </Box>
     </Box>
   );
