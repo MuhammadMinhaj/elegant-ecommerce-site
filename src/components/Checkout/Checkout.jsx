9390ae0477db8420000844ebeb1e0709b7bec32f
Muhammad Minhaj <developer.mdminhaj@gmail.com>
2022-04-11 16:55:03 +0600

    Completed checkout page

--- src/components/Checkout/Checkout.jsx
+++ src/components/Checkout/Checkout.jsx
@@ -3,12 +3,15 @@ import AdditionalInformation from './AdditionalInformation';
 import BillingDetail from './BillingDetail';
 import Cart from './Cart/Cart';
 import PaymentMethod from './PaymentMethod/PaymentMethods';
+import UnauthenticatedButtons from './PaymentMethod/UnauthenticatedButtons';
 import ShippingDetail from './ShippingDetail';
 import ShippingMethod from './ShippingMethod';
 
 function Checkout() {
   return (
     <Box className="container" py="2rem">
+      <UnauthenticatedButtons />
+      <Box height="1rem" />
       <Grid container spacing={2}>
         <Grid item md={12} lg={6}>
           <Box bgcolor="var(--white)" p="2rem" borderRadius="0.5rem">
--- src/components/Checkout/PaymentMethod/PaymentMethods.jsx
+++ src/components/Checkout/PaymentMethod/PaymentMethods.jsx
@@ -1,19 +1,15 @@
-import CalendarTodayIcon from '@mui/icons-material/CalendarToday';
 import CreditCardIcon from '@mui/icons-material/CreditCard';
-import DriveFileRenameOutlineIcon from '@mui/icons-material/DriveFileRenameOutline';
-import NumbersIcon from '@mui/icons-material/Numbers';
 import {
-  Box, Grid, Typography
+  Box, Button, Grid, Typography
 } from '@mui/material';
-import { useRef, useState } from 'react';
+import { useState } from 'react';
 import CustomButton from '../../Common/CustomButton';
 import icons from '../../Icons';
-import InputField from '../../User/Common/InputField';
+import CreditCardInfo from './CreditCardInfo';
 
 const { PaypalIcon } = icons;
+
 export default function PaymentMethods({ errors, handleChange }) {
-  const fullNameRef = useRef();
-  const emailRef = useRef();
   const [isCreditCard, setCreditCard] = useState(false);
   const [isPaypal, setPaypal] = useState(false);
   const handleClick = (isUps) => {
@@ -33,59 +29,19 @@ export default function PaymentMethods({ errors, handleChange }) {
       </Typography>
       <Box height="1rem" />
       <Grid container spacing={2}>
-
-        <Grid item xs={12} md={12} lg={6}>
-          <InputField
-            inputRef={fullNameRef}
-            name="lastName"
-            label="Card Number"
-            icon={<NumbersIcon />}
-            placeholder="0000 0000 0000 0000"
-            handleChange={handleChange}
-            errorMsg={errors?.fullName}
-          />
-        </Grid>
-        <Grid item xs={12} md={12} lg={6}>
-          <InputField
-            inputRef={fullNameRef}
-            name="firstname"
-            label="Card Holder Name"
-            icon={<DriveFileRenameOutlineIcon />}
-            placeholder="John Lewis"
-            handleChange={handleChange}
-            errorMsg={errors?.fullName}
-          />
-        </Grid>
-        <Grid item xs={12} md={12} lg={6}>
-          <InputField
-            inputRef={emailRef}
-            name="email"
-            label="Expire Date"
-            icon={<CalendarTodayIcon />}
-            placeholder="Year"
-            handleChange={handleChange}
-            errorMsg={errors?.email}
-          />
-        </Grid>
-        <Grid item xs={12} md={12} lg={6}>
-          <InputField
-            inputRef={emailRef}
-            name="85254"
-            label="CVV"
-            icon={<NumbersIcon />}
-            placeholder="000"
-            handleChange={handleChange}
-            errorMsg={errors?.email}
-            type="number"
-          />
-        </Grid>
         <Grid item xs={12} sm={12} md={6}>
           <CustomButton title="Credit Card" subTitle="Easy payment way" isChecked={isCreditCard} handleClick={() => handleClick(true)} icon={<CreditCardIcon />} />
         </Grid>
         <Grid item xs={12} sm={12} md={6}>
           <CustomButton title="Paypal" subTitle="Easy payment way" isChecked={isPaypal} handleClick={() => handleClick(false)} icon={<PaypalIcon />} />
         </Grid>
+        {
+          isCreditCard && <CreditCardInfo errors={errors} handleChange={handleChange} />
+        }
+
       </Grid>
+      <Box height="1rem" />
+      <Button variant="contained" color="warning" fullWidth>Place Order</Button>
 
     </Box>
 
