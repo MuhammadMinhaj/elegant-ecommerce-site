97b7d6ab845e0f8641c1e86e8f758c463dfe3c9b
Muhammad Minhaj <developer.mdminhaj@gmail.com>
2022-04-11 22:29:48 +0600

    feat(): Completed user profile and edit profile

--- src/components/User/Profile/Profile.jsx
+++ src/components/User/Profile/Profile.jsx
@@ -1,35 +1,68 @@
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import EditIcon from '@mui/icons-material/Edit';
 import LocalShippingIcon from '@mui/icons-material/LocalShipping';
 import PaymentsIcon from '@mui/icons-material/Payments';
 import PersonSharpIcon from '@mui/icons-material/PersonSharp';
 import ShoppingBagOutlinedIcon from '@mui/icons-material/ShoppingBagOutlined';
-import { Box, Divider, Grid } from '@mui/material';
+import {
+  Box, Button, Divider, Grid
+} from '@mui/material';
+import { useState } from 'react';
 import Title from '../Common/Title';
 import Layout from '../Layout/Layout';
 import CardItem from './CardItem';
+import ProfileEdit from './ProfileEdit';
+import ProfileInfoList from './ProfileInfoList';
 import User from './User';
 
+function ProfileShow() {
+  return (
+    <>
+      <Box p="1rem">
+        <Grid container spacing={2}>
+          <Grid item xs={12} sm={12} md={12} lg={6}>
+            <User />
+          </Grid>
+          <Grid item xs={6} sm={6} md={4} lg={2}>
+            <CardItem count="7" title="All Orders" icon={<ShoppingBagOutlinedIcon fontSize="inherit" color="inherit" />} />
+          </Grid>
+          <Grid item xs={6} sm={6} md={4} lg={2}>
+            <CardItem count="4" title="Awaiting Payments" icon={<PaymentsIcon fontSize="inherit" color="inherit" />} />
+          </Grid>
+          <Grid item xs={12} sm={6} md={4} lg={2}>
+            <CardItem count="7" title="Awaiting Delivery" icon={<LocalShippingIcon fontSize="inherit" color="inherit" />} />
+          </Grid>
+        </Grid>
+      </Box>
+      <ProfileInfoList />
+    </>
+  );
+}
+
 function Profile() {
+  const [isEdit, setEdit] = useState(true);
   return (
     <Layout currentPath="profile">
       <Box bgcolor="var(--white)" p="0.5rem" borderRadius="0.5rem">
-        <Title title="My Profile" icon={<PersonSharpIcon sx={{ color: 'inherit', fontSize: 'inherit' }} />} />
+        <Title
+          title={isEdit ? 'Edit Profile' : 'My Profile'}
+          icon={<PersonSharpIcon sx={{ color: 'inherit', fontSize: 'inherit' }} />}
+          actions={(
+            <Button
+              variant="text"
+              startIcon={isEdit ? <ArrowBackIcon /> : <EditIcon />}
+              color="warning"
+              size="large"
+              onClick={() => setEdit(!isEdit)}
+            >
+              {isEdit ? 'Back To Profile' : 'Edit Profile'}
+            </Button>
+          )}
+        />
         <Divider />
-        <Box p="1rem">
-          <Grid container spacing={2}>
-            <Grid item xs={12} sm={12} md={12} lg={6}>
-              <User />
-            </Grid>
-            <Grid item xs={6} sm={6} md={4} lg={2}>
-              <CardItem count="7" title="All Orders" icon={<ShoppingBagOutlinedIcon fontSize="inherit" color="inherit" />} />
-            </Grid>
-            <Grid item xs={6} sm={6} md={4} lg={2}>
-              <CardItem count="4" title="Awaiting Payments" icon={<PaymentsIcon fontSize="inherit" color="inherit" />} />
-            </Grid>
-            <Grid item xs={12} sm={6} md={4} lg={2}>
-              <CardItem count="7" title="Awaiting Delivery" icon={<LocalShippingIcon fontSize="inherit" color="inherit" />} />
-            </Grid>
-          </Grid>
-        </Box>
+        {
+          isEdit ? <ProfileEdit /> : <ProfileShow />
+        }
 
       </Box>
     </Layout>
