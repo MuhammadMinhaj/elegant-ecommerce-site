4ebf0d66ad2b8e75bf1b42294c4a514607da8176
Muhammad Minhaj <developer.mdminhaj@gmail.com>
2022-04-16 03:18:38 +0600

    style(): Improve UI and Position

--- src/components/DynamicCategory/Product/Item/Actions.jsx
+++ src/components/DynamicCategory/Product/Item/Actions.jsx
@@ -3,51 +3,53 @@ import FavoriteBorderOutlinedIcon from '@mui/icons-material/FavoriteBorderOutlin
 import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
 import VisibilityOutlinedIcon from '@mui/icons-material/VisibilityOutlined';
 import { Box, CardActionArea } from '@mui/material';
+import Tooltip from '@mui/material/Tooltip';
 import { useRouter } from 'next/router';
 
-function ActionButton({ icon, handleClick }) {
+function ActionButton({
+  icon, handleClick, name, isSmall
+}) {
   return (
-    <Box
-      px="0.5rem"
-      borderRadius="50%"
-      sx={{
-        overflow: 'hidden'
-      }}
-    >
-      <CardActionArea onClick={handleClick}>
+    <Tooltip title={name || 'N/A'} placement="right-start">
+      <Box
+        p="0.25rem"
+      >
+        <CardActionArea onClick={handleClick} sx={{ borderRadius: '50%' }}>
 
-        <Box
-          boxShadow={1}
-          p="0.35rem"
-          borderRadius="50%"
-          sx={{
-            '&:hover': {
-              background: 'black',
-              color: 'var(--white)',
-              cursor: 'pointer'
-            },
-            overflow: 'hidden'
-          }}
-        >
+          <Box
+            boxShadow={1}
+            p={isSmall ? '0.15rem' : '0.35rem'}
+            borderRadius="50%"
+            sx={{
+              '&:hover': {
+                background: 'var(--white)',
+                color: 'var(--secondary)',
+                cursor: 'pointer'
+              },
+              overflow: 'hidden',
+              background: 'var(--secondary)',
+              color: 'var(--white)'
+            }}
+          >
+            {icon}
+          </Box>
+        </CardActionArea>
 
-          {icon}
+      </Box>
+    </Tooltip>
 
-        </Box>
-      </CardActionArea>
-
-    </Box>
   );
 }
 
-function Actions({ position }) {
+function Actions({ isSmall }) {
   const router = useRouter();
 
   return (
-    <Box display="flex" alignItems="center" justifyContent={position || 'center'}>
-      <ActionButton icon={<ShoppingCartOutlinedIcon />} />
-      <ActionButton icon={<VisibilityOutlinedIcon />} handleClick={() => router.push('/product/demo')} />
-      <ActionButton icon={<CompareArrowsIcon />} />
-      <ActionButton icon={<FavoriteBorderOutlinedIcon />} />
+    <Box borderRadius="1rem">
+      <ActionButton isSmall={isSmall} name="Add to cart" icon={<ShoppingCartOutlinedIcon />} />
+      <ActionButton isSmall={isSmall} name="View this" icon={<VisibilityOutlinedIcon />} handleClick={() => router.push('/product/demo')} />
+      <ActionButton isSmall={isSmall} name="Compare this" icon={<CompareArrowsIcon />} />
+      <ActionButton isSmall={isSmall} name="Add to wishlist" icon={<FavoriteBorderOutlinedIcon />} />
     </Box>
   );
 }
