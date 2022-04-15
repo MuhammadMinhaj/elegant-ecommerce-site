477010752dcc006f166a0281d7177e83870cc540
Muhammad Minhaj <developer.mdminhaj@gmail.com>
2022-04-15 16:26:58 +0600

    fix(): Updated item to make more responsive

--- src/components/Shop/TrendingProducts/Item.jsx
+++ src/components/Shop/TrendingProducts/Item.jsx
@@ -5,19 +5,47 @@ function Item({
 }) {
   return (
 
-    <Box display="flex" width="100%" alignItems="center">
+    <Box
+      display="flex"
+      width="100%"
+      alignItems="center"
+      border="1px solid var(--white)"
+      sx={{
+        '&:hover': {
+          border: '1px solid var(--white-border)',
+          transition: 'all 0.25s'
+        }
+
+      }}
+    >
       <Box sx={{
         backgroundImage: `url("${images[0]}")`,
         backgroundSize: 'cover',
         backgroundPosition: 'center',
         height: '80px',
         width: '80px'
+
       }}
       >
         {/* <Box component="img" src={imgSrc} maxWidth="80px" maxHeight="auto" width="100%" /> */}
       </Box>
       <Box flexGrow="1">
-        <Typography sx={{ lineHeight: '1' }} variant="subtitle1" color="var(--text-head)">{title}</Typography>
+        <Typography
+          sx={{
+            lineHeight: '1',
+            '&:hover': {
+              color: 'var(--secondary)',
+              transition: 'all 0.25s',
+              cursor: 'pointer'
+
+            }
+          }}
+          variant="subtitle1"
+          color="var(--text-head)"
+        >
+          {title}
+
+        </Typography>
         <Typography sx={{ lineHeight: '1' }} variant="h6" color={discount ? 'red' : 'var(--primary)'}>{price}</Typography>
         {
           discount && (
