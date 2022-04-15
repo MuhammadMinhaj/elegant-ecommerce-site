52358f84aec8bd74d853ff1aa4834881a96e46e8
Muhammad Minhaj <developer.mdminhaj@gmail.com>
2022-04-15 20:07:55 +0600

    feat(): Added react-viewer to view the image on the product

--- src/components/SingleProduct/ProductImage.jsx
+++ src/components/SingleProduct/ProductImage.jsx
@@ -7,9 +7,14 @@ import {
   useMediaQuery,
   useTheme
 } from '@mui/material';
+import dynamic from 'next/dynamic';
 import Image from 'next/image';
 import { useEffect, useState } from 'react';
-import { SideBySideMagnifier } from 'react-image-magnifiers';
+
+const ReactViewer = dynamic(
+  () => import('react-viewer'),
+  { ssr: false }
+);
 
 function ImageItem({ path, getImgIndex, ind }) {
   return (
@@ -38,6 +43,8 @@ function Product({ images, name }) {
   const [animate, setAnimate] = useState(false);
   const theme = useTheme();
   const isMD = useMediaQuery(theme.breakpoints.down('md'));
+  const [visible, setVisible] = useState(false);
+
   useEffect(() => {
     setAnimate(false);
 
@@ -52,19 +59,26 @@ function Product({ images, name }) {
   const getImgIndex = (ind) => {
     setImgIndex(ind);
   };
+  const tempImages = images.map((img) => (
+    {
+      src: img,
+      alt: ''
+    }
+  ));
   return (
     <Box height="100%" display="flex" flexDirection="column" justifyContent="space-between">
+
+      <ReactViewer
+        visible={visible}
+        onClose={() => { setVisible(false); }}
+        images={tempImages}
+        zIndex={99999}
+        activeIndex={imgIndex}
+      />
+
       {images && (
         <Grow in={animate}>
-          <Box>
-            <SideBySideMagnifier
-              imageSrc={images[imgIndex]}
-              imageAlt="single product"
-              largeImageSrc={images[imgIndex]} // Optional
-
-            />
-          </Box>
-
+          <Box component="img" sx={{ cursor: 'pointer' }} src={images[imgIndex]} onClick={() => setVisible(true)} width="100%" height="auto" />
         </Grow>
       )}
 
@@ -76,99 +90,3 @@ function Product({ images, name }) {
   );
 }
 export default Product;
-
-// import {
-//   Box,
-//   CardActionArea,
-//   Divider,
-//   Grow,
-//   Paper,
-//   useMediaQuery,
-//   useTheme
-// } from '@mui/material';
-// import Image from 'next/image';
-// import { useEffect, useState } from 'react';
-// import ReactImageMagnify from 'react-image-magnify';
-
-// function ImageItem({ path, getImgIndex, ind }) {
-//   return (
-//     <Box mx="0.25rem" width="100%" maxWidth="80px">
-//       <CardActionArea onClick={() => getImgIndex(ind)}>
-//         <Paper variant="outlined">
-//           {path && <Image src={path} width={100} height={100} sizes={100} />}
-//         </Paper>
-//       </CardActionArea>
-//     </Box>
-//   );
-// }
-
-// function ImageItemContainer({ getImgIndex, images }) {
-//   return (
-//     <Box display="flex" justifyContent="flex-start" mt="1rem">
-//       {images?.map((img, ind) => (
-//         <ImageItem key={img} path={img} getImgIndex={getImgIndex} ind={ind} />
-//       ))}
-//     </Box>
-//   );
-// }
-
-// function Product({ images, name }) {
-//   const [imgIndex, setImgIndex] = useState(0);
-//   const [animate, setAnimate] = useState(false);
-//   const theme = useTheme();
-//   const isMD = useMediaQuery(theme.breakpoints.down('md'));
-//   useEffect(() => {
-//     setAnimate(false);
-
-//     const intervalCancel = setInterval(() => {
-//       setAnimate(true);
-//     }, 500);
-//     return () => {
-//       clearInterval(intervalCancel);
-//     };
-//   }, [imgIndex]);
-
-//   const getImgIndex = (ind) => {
-//     setImgIndex(ind);
-//   };
-//   return (
-//     <Box height="100%" display="flex" flexDirection="column" justifyContent="space-between">
-//       {images && (
-//         <Grow in={animate}>
-//           <Box>
-//             <ReactImageMagnify
-//               {...{
-//                 smallImage: {
-//                   alt: name || 'N/A',
-//                   isFluidWidth: true,
-//                   src: images[imgIndex],
-//                   sizes: '(max-width: 480px) 100vw, (max-width: 1200px) 30vw, 360px'
-//                 },
-//                 largeImage: {
-//                   src: images[imgIndex],
-//                   width: 1200,
-//                   height: 1800
-//                 },
-//                 enlargedImageContainerDimensions: {
-//                   width: '100%',
-//                   height: '100%'
-//                 },
-//                 enlargedImagePosition: isMD ? 'over' : 'beside'
-
-//               }}
-//               style={{ zIndex: '99' }}
-//             />
-
-//           </Box>
-
-//         </Grow>
-//       )}
-
-//       <Box>
-//         <Divider />
-//         <ImageItemContainer getImgIndex={getImgIndex} images={images} />
-//       </Box>
-//     </Box>
-//   );
-// }
-// export default Product;
